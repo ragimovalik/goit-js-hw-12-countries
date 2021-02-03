f50a8640de81c69d88107b209701afd81863a305
ragimovalik <ragimovalik@gmail.com>
2021-02-03 19:05:42 +0200

    Notify fixes

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -1,5 +1,6 @@
-import { notice, info, error, defaults } from '@pnotify/core';
-import '@pnotify/core/dist/BrightTheme.css';
+import { notice, error, defaults, Stack } from '@pnotify/core';
+// import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/Material.css';
 import 'material-design-icons/iconfont/material-icons.css';
 
